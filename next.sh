5563947df430cfd107362e54a80f458d99631c59
Christian Wolf <christian.wolf1@arvato.com>
2017-01-17 13:46:44 +0100

    initial commit

---
+++ next.sh
@@ -0,0 +1,13 @@
+branch_name="$(git symbolic-ref HEAD 2>/dev/null)" ||
+branch_name="(unnamed branch)"     # detached HEAD
+branch_name=${branch_name##refs/heads/}
+echo $branch_name
+if [ "$branch_name" == "master" ]
+then
+  next_branch_name=1
+else
+  next_branch_name="$((branch_name + 1))"  	
+fi
+
+git checkout $next_branch_name
+#echo $next_branch_name
