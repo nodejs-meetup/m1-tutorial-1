b6311410c46551c04431cf745a03d1b5b47cf6f6
Christian Wolf <christian.wolf1@arvato.com>
2017-01-17 21:44:01 +0100

    added body-parser

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const restApi = require('./lib/restApi')
+const bodyParser = require('body-parser')
 const config = require('./config')
 
 let app = express()
+app.use(bodyParser.json())
 restApi(app)
 
 app.listen(config.port)
